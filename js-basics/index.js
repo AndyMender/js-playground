e213cf7dcb7469590e2c469e69d468b2081d0544
Andy Mender <andymenderunix@gmail.com>
2022-10-23 22:50:52 +0200

    Add some primitives

--- js-basics/index.js
+++ js-basics/index.js
@@ -3,3 +3,13 @@ let firstName = "Andy";
 let lastName = "Mender";
 console.log(firstName);
 console.log(lastName);
+
+// basic constant declaration
+const interestRate = 0.3;
+
+// all primitive types are declared the same way
+let age = 30;
+let senseOfHumor = 0.5;
+let isApproved = true;
+let mainInterest = undefined;       // no value set (yet?)
+let favoriteColor = null;           // setting an empty value or clearing
