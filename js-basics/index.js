8ffcd2c9d2a4b23ed978406e78198abbf017f09e
Andy Mender <andymenderunix@gmail.com>
2022-10-23 22:43:51 +0200

    Add some var declarations

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,1 +1,5 @@
-console.log("Hello World");
+// JS prefers camelCase for variable names and separate lines for each declaration
+let firstName = "Andy";
+let lastName = "Mender";
+console.log(firstName);
+console.log(lastName);
