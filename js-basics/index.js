49a6d3bee6392a79a01b1be02546bb28667f8c92
Andy Mender <andymenderunix@gmail.com>
2022-10-24 21:31:57 +0200

    Add simple type checks

--- js-basics/index.js
+++ js-basics/index.js
@@ -13,3 +13,12 @@ let senseOfHumor = 0.5;
 let isApproved = true;
 let mainInterest = undefined;       // no value set (yet?)
 let favoriteColor = null;           // setting an empty value or clearing
+
+// Type checking
+// Integers and floats both count as floats of type 'number'
+console.log(typeof age);
+console.log(typeof senseOfHumor === "number");
+// 'undefined' is a single-value type
+console.log(typeof mainInterest);
+// null counts as type 'object'
+console.log(typeof favoriteColor);
