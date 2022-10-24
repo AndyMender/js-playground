08c8fd97e203d61b5978cef9e5b3e69659ca95c7
Andy Mender <andymenderunix@gmail.com>
2022-10-24 21:50:30 +0200

    Add object accessors

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,6 +1,6 @@
 // JS prefers camelCase for variable names and separate lines for each declaration
-let firstName = "Andy";
-let lastName = "Mender";
+let firstName = 'Andy';
+let lastName = 'Mender';
 console.log(firstName);
 console.log(lastName);
 
@@ -17,8 +17,21 @@ let favoriteColor = null;           // setting an empty value or clearing
 // Type checking
 // Integers and floats both count as floats of type 'number'
 console.log(typeof age);
-console.log(typeof senseOfHumor === "number");
+console.log(typeof senseOfHumor === 'number');
 // 'undefined' is a single-value type
 console.log(typeof mainInterest);
 // null counts as type 'object'
 console.log(typeof favoriteColor);
+
+// Objects
+let person = {
+    name: 'John',
+    age: 30,
+};
+console.log(person);
+
+// Object accessors
+person.name = 'Alan';
+console.log(person.name);
+person['name'] = 'John';    // key lookup supports also variables
+console.log(person['name']);
