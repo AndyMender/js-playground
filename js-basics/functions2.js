a7f206c45bd9328ac14189f6ab4d0b94f1beb055
Andy Mender <andymenderunix@gmail.com>
2023-12-16 18:59:23 +0100

    Add more functions

--- js-basics/functions2.js
+++ js-basics/functions2.js
@@ -1,6 +1,7 @@
 // Javascript doesn't care about the type or number of variables.
 // Missing values are set to 'undefined', excess values are ignored
 function sum(...args) {
+    // NOTE: '.reduce()' can be used also without the 'total' parameter
     let total = (typeof args[0] === 'number') ? 0 : '';
     return args.reduce((a, b) => a + b, total);
 }
@@ -19,3 +20,9 @@ function sum2() {
 
 console.log(sum2(1, 2));
 console.log(sum2(1, 2, 3, 5));
+
+function discount_sum(total_discount, ...prices) {
+    const total = prices.reduce((a, b) => a + b);
+    return total * (1 - total_discount);
+}
+console.log(discount_sum(0.1, 20, 30));
