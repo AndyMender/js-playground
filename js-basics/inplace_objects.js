5dbd9dbb81680ae517e6e9bc30f14a6c765c7431
Andy Mender <andymenderunix@gmail.com>
2024-03-04 19:48:34 +0100

    Extend playground for object and scoping

--- js-basics/inplace_objects.js
+++ js-basics/inplace_objects.js
@@ -7,12 +7,12 @@ const video = {
     },
     showTags() {
         // 'forEach' uses the global scope inside the passed function by default.
-        // We need to explicit set it to the 'this' of the local object to fix scoping.
-        // NOTE: The for-of loop would be simpler, since it doesn't alter the scope
+        // However, arrow functions respect local scope!
         this.tags.forEach((tag) => {
             console.log(`'${this.title}' - '${tag}'`);
-        }, this);
+        });
 
+        // NOTE: The for-of loop would be simpler, since it doesn't alter the scope
         // for (let tag of this.tags)
         //   console.log(`'${this.title}' - '${tag}'`);
     }
@@ -32,6 +32,12 @@ function playVideo() {
     console.log(`Playing global video ${this}...`);
 }
 playVideo();
+// NOTE: it's possible inject a custom 'this' object even to a regular function call
+playVideo.call({ title: 'Black Jack - 2', director: 'Hideyoshi Takanaka' });
+playVideo.apply({ title: 'Black Jack - 2', director: 'Hideyoshi Takanaka' });
+// 'bind' generates a new function - useful in Array methods requiring a callable or in factory functions
+const playVideoCustom = playVideo.bind({ title: 'Black Jack - 2', director: 'Hideyoshi Takanaka' });
+
 
 // Below is treated like a class constructor so 'this' refers to an object instance
 function Video(title) {
