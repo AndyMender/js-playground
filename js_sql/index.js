f8f7ea1987d3eed73dd048d7d1be94496f61b891
Andy Mender <andymenderunix@gmail.com>
2024-04-29 13:24:48 +0200

    Add sample node.js code with SQLite query

---
+++ js_sql/index.js
@@ -0,0 +1,17 @@
+const sqlite3 = require('sqlite3');
+
+function printQueryResults(rows) {
+    console.log(rows);
+}
+
+// Create or load existing database file
+const db = new sqlite3.Database('./db.sqlite');
+
+// Run full query + handle results in a callback
+// NOTE: For now the table doesn't exist
+db.all('SELECT * FROM TemperatureData ORDER BY year;', (error, rows) => {
+  if (error) {
+    throw error;
+  }
+  printQueryResults(rows);
+});
