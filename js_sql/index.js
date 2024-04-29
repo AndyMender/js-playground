8d62d4e88b64dc2f0ab7a9fc252ae8f2bc04c8cd
Andy Mender <andymenderunix@gmail.com>
2024-04-29 14:39:06 +0200

    Add single row query code

--- js_sql/index.js
+++ js_sql/index.js
@@ -9,9 +9,23 @@ const db = new sqlite3.Database('./db.sqlite');
 
 // Run full query + handle results in a callback
 // NOTE: For now the table doesn't exist
-db.all('SELECT * FROM TemperatureData ORDER BY year;', (error, rows) => {
-  if (error) {
-    throw error;
-  }
-  printQueryResults(rows);
-});
+db.all(
+    'SELECT * FROM TemperatureData ORDER BY year;',
+    (error, rows) => {
+        if (error) {
+            throw error;
+        }
+        printQueryResults(rows);
+    }
+);
+
+// Limited single row query
+db.get(
+    'SELECT * FROM TemperatureData WHERE year=1989',
+    (error, row) => {
+        if (error) {
+            throw error;
+        }
+        printQueryResults(row);
+    }
+);
