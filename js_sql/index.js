0dc8c3cc77e89ff17a1e306bd7c9b68ca2175547
Andy Mender <andymenderunix@gmail.com>
2024-04-29 15:25:37 +0200

    Add some table creation code and expand INSERT call

--- js_sql/index.js
+++ js_sql/index.js
@@ -7,12 +7,38 @@ function printQueryResults(rows) {
 // Create or load existing database file
 const db = new sqlite3.Database('./db.sqlite');
 
+// Non-query SQL commands
+db.run(
+    'CREATE TABLE IF NOT EXISTS TemperatureData (id INTEGER PRIMARY KEY AUTOINCREMENT, location TEXT, year INT, temp_avg REAL)',
+    (error) => {
+        if(error) throw error;
+    }
+);
+
+const newRow = {
+  location: 'Istanbul, Turkey',
+  year: 1976,
+  avgTemp: 13.35
+}
+
+db.run(
+    'INSERT INTO TemperatureData (location, year, temp_avg) VALUES ($location, $year, $avgTemp)',
+    {
+        $location: newRow.location,
+        $year: newRow.year,
+        $avgTemp: newRow.avgTemp
+    },
+    function (error) {
+        console.log(this.lastID);
+    }
+);
+
 // Run full query + handle results in a callback
-// NOTE: For now the table doesn't exist
 db.all(
     'SELECT * FROM TemperatureData ORDER BY year;',
     (error, rows) => {
         if (error) {
+            // Or log the error and move on
             throw error;
         }
         printQueryResults(rows);
@@ -46,4 +72,3 @@ for (const id of ids) {
         }
     );
 }
-
